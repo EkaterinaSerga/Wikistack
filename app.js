868f6019c98f22fb8437d69203be0282488fbcfb
Ekaterina Serga <ekaterina.a.serga@gmail.com>
2020-04-30 12:52:35 -0400

    Introduction and Setup complete

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const morgan = require('morgan');
+const pg = require('pg');
+const Sequelize = require('sequelize');
+const layout = require('./views/layout')
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(express.static(__dirname + '/public'));
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+  res.send(layout());
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`We are listening on ${PORT}, hoorah!`);
+});
+
+
